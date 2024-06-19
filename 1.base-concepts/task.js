39124e1cf68e257f5f1f6c1e155358ab77287915
yulialener <lener.yu@yandex.ru>
2024-06-19 16:49:24 +0700

    Change code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,8 +4,8 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
   if (d > 0) {
-      arr[0] = (-b + Math.sqrt(d)) / (2 * a);
-      arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
   } else if (d === 0) {
     arr[0] = -b / (2 * a);
   } else {
@@ -36,7 +36,4 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     console.log(totalAmount);
     return +totalAmount;
   }
-}
-
-return +totalAmount;
 }
